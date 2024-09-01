e83f9ecb805232ffe5788df72ada3c8c22f394b7
Bhavd33p <165815536+Bhavd33p@users.noreply.github.com>
2024-09-01 14:59:04 +0530

    Add files via upload

---
+++ Cp1.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+#include <iostream>
+#include<set>
+#include<map>
+using namespace std;
+#define in(x) cin>>x
+#define out(x) cout<<x<<"\n"
+const long long MOD = 1000000007;
+#define pb(x) push_back(x)
+#define pk pop_back()
+#define ll long long 
+#define f(i,a,b) for(ll i=a;i<b;i++)
+#define fast() ios_base::sync_with_stdio(false); cin.tie(NULL) cout.tie(NULL)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define mp make_pair
+#define F first
+#define S second
+#define Sort(a) sort(a.begin(),a.end())
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>    pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+void solve() {
+  ll i, j;
+  cin>>i>>j;
+  cout<<i+j<<endl;
+   
+}
+int main() {
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+    int t = 1;
+    cin >> t;
+    while(t--) {
+      solve();
+    }
+
+    return 0;
+}
